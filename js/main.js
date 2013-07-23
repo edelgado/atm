3885fde7c4758738ca778572c58604ab82dbe250
Enrique Delgado <delgado.enrique@gmail.com>
2013-07-23 18:29:32 -0500

    Spending too much time, lets go old school.

--- js/main.js
+++ js/main.js
@@ -2,21 +2,26 @@
 var atm = new ATM({});
 
 /* Create credit card neon signs */
+/*
 var cc1 = new CC({kind: 'star'});
 var cc2 = new CC({kind: 'pulse'});
 var cc3 = new CC({kind: 'maestro'});
 var cc4 = new CC({kind: 'mastercard'});
 var cc5 = new CC({kind: 'plus'});
 var cc6 = new CC({kind: 'visa'});
+*/
 /* Next step: create a collection and use a view that expects a collection: var ccs = new CCCollection([cc1, cc2, cc3, cc4, cc5, cc6]); */
+/*
 var ccView1 = new CCView({model: cc1, el: $('#cc1')});
 var ccView2 = new CCView({model: cc2, el: $('#cc2')});
 var ccView3 = new CCView({model: cc3, el: $('#cc3')});
 var ccView4 = new CCView({model: cc4, el: $('#cc4')});
 var ccView5 = new CCView({model: cc5, el: $('#cc5')});
 var ccView6 = new CCView({model: cc6, el: $('#cc6')});
+*/
 
 /* Create ATM buttons */
+/*
 var btn1 = new MenuButton({enabled: 'yes', slot: 'a'});
 var btn2 = new MenuButton({enabled: 'yes', slot: 'b'});
 var btn3 = new MenuButton({enabled: 'yes', slot: 'c'});
@@ -33,6 +38,7 @@ var btnView5 = new ATMButtonView({model: btn5, el: $('#btn5')});
 var btnView6 = new ATMButtonView({model: btn6, el: $('#btn6')});
 var btnView7 = new ATMButtonView({model: btn7, el: $('#btn7')});
 var btnView8 = new ATMButtonView({model: btn8, el: $('#btn8')});
+*/
 
 /*
 var op1 = new MenuOption({
