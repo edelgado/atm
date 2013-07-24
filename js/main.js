a3c3b841c07c0bc121c7bd8d0c8f174830aaaf74
Enrique Delgado <delgado.enrique@gmail.com>
2013-07-23 19:50:25 -0500

    Spaces instead of tabs and finishing functionality.

--- js/main.js
+++ js/main.js
@@ -2,95 +2,110 @@
 var atm = (function ($) {
 
   // Private varibales:
-	var my = {},
-		balance = 1000,
-		secret = 1234,
-		loggedIn = false;
+  var my = {},
+    balance = 1000,
+    secret = 1234,
+    loggedIn = false;
 
   // Private methods:
-	function enableButton(slot, label) {
-		$('#lbl-' + slot).html(label);
-		$('#lbl-ln-' + slot).addClass('active');
-		$('#key-' + slot).addClass('active');
-	}
+  function enableButton(slot, label) {
+    $('#lbl-' + slot).html(label);
+    $('#lbl-ln-' + slot).addClass('active');
+    $('#key-' + slot).addClass('active');
+  }
 
-	function disableButton(slot) {
-		$('#lbl-' + slot).html('');
-		$('#lbl-ln-' + slot).removeClass('active');
-		$('#key-' + slot).removeClass('active');
-	}
-
+  function disableButton(slot) {
+    $('#lbl-' + slot).html('');
+    $('#lbl-ln-' + slot).removeClass('active');
+    $('#key-' + slot).removeClass('active');
+  }
+  
   // Public methods:
-	my.getBalance = function() {
-  	return balance;
-	}
-	
-	my.deposit = function(amount) {
-	  if (typeof(amount) == 'undefined' || isNaN(amount)) return;
-  	balance += Math.abs(amount);
-  	return balance;
-	}
+  my.getBalance = function() {
+    return balance;
+  }
+  
+  my.deposit = function(amount) {
+    if (typeof(amount) == 'undefined' || isNaN(amount)) return;
+    balance += Math.abs(amount);
+    return balance;
+  }
 
-	my.withdraw = function(amount) {
-	  if (typeof(amount) == 'undefined' || isNaN(amount) || balance < amount) return;
-  	balance -= Math.abs(amount);
-  	return balance;
-	}
+  my.withdraw = function(amount) {
+    if (typeof(amount) == 'undefined' || isNaN(amount) || balance < amount) return;
+    balance -= Math.abs(amount);
+    return balance;
+  }
 
-	my.getPin = function() {
-	  var enteredPin = prompt('Hint: 1234');
-	  if (enteredPin == secret) {
-  	  $('#message').html('Please make a choice...');
-  	  enableButton('c', 'Withdraw');
-  	  enableButton('d', 'Deposit');
-  	  enableButton('g', 'Balance');
-  	  enableButton('h', 'Re-Enter PIN');
-  	  loggedIn = true;
-	  } else {
-  	  $('#message').html('Incorrect PIN.');
-  	  setTimeout(function(){
-    	  $('#message').html('Welcome');
-    	  disableButton('c');
-    	  disableButton('d');
-    	  disableButton('g');
-    	  enableButton('h', 'Enter PIN');
-  	  }, 1000);
-  	  loggedIn = false;
-	  }
-	}
-	
-	my.showBalance = function() {
-  	$('#message').html('Your balance is: ' + balance);
-	}
-	
-	// Events
+  my.getPin = function() {
+    var enteredPin = prompt('Hint: 1234');
+    if (enteredPin == secret) {
+      $('#message').html('Please make a choice...');
+      enableButton('c', 'Withdraw');
+      enableButton('d', 'Deposit');
+      enableButton('g', 'Balance');
+      enableButton('h', 'Re-Enter PIN');
+      $('span.cc.pulse').addClass('active');
+      loggedIn = true;
+    } else {
+      $('#message').html('Incorrect PIN.');
+      setTimeout(function(){
+        $('#message').html('Welcome');
+        disableButton('c');
+        disableButton('d');
+        disableButton('g');
+        enableButton('h', 'Enter PIN');
+        $('span.cc.pulse').removeClass('active');
+      }, 1000);
+      loggedIn = false;
+    }
+  }
+  
+  my.showBalance = function() {
+    $('#message').html('Your balance is: ' + balance);
+  }
+  
+  // Events
 
-	$('#key-c').click(function(e){
-	  if (loggedIn) {
-  	  var amt = prompt('Amount:');
-  	  my.withdraw(amt);
-  	  my.showBalance();
-	  }
-	});
+  // Withdraw key
+  $('#key-c').click(function(e){
+    if (loggedIn) {
+      var amt = prompt('Amount:');
+      my.withdraw(amt);
+      my.showBalance();
+      setTimeout(function(){
+        $('#message').html('Please make a choice...');
+      }, 1000);
+    }
+  });
 
-	$('#key-d').click(function(e){
-	  if (loggedIn) {
-  	  var amt = prompt('Amount:');
-  	  my.deposit(amt);
-  	  my.showBalance();
-	  }
-	});	
+  // Deposit key
+  $('#key-d').click(function(e){
+    if (loggedIn) {
+      var amt = prompt('Amount:');
+      my.deposit(amt);
+      my.showBalance();
+      setTimeout(function(){
+        $('#message').html('Please make a choice...');
+      }, 1000);
+    }
+  });  
 
-	$('#key-h').click(function(e){
-  	my.getPin();
-	});
+  // Enter PIN key
+  $('#key-h').click(function(e){
+    my.getPin();
+  });
 
-	$('#key-g').click(function(e){
-	  if (loggedIn) {
-  	  my.showBalance();
-	  }
-	});
-
-
-	return my;
+  // Balance key
+  $('#key-g').click(function(e){
+    if (loggedIn) {
+      my.showBalance();
+      setTimeout(function(){
+        $('#message').html('Please make a choice...');
+      }, 1000);
+    }
+  });
+  
+  return my;
+  
 }($));
